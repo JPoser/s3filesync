5977e5869ed5e2d2e7541b7c4c3f80cb658e6c51
Joe Poser <joe.poser@gmail.com>
2015-08-20 17:45:32 +0100

    First commit of script
    First commit of S3 file sync script. All references to file locations
    have been removed and config variables added instead. Initial testing
    sucessful.

---
+++ s3logsync.py
@@ -0,0 +1,25 @@
+
+import subprocess
+import logbook
+from getpass import getuser
+
+
+cfg_s3folder = 
+cfg_localstorage = 
+cfg_logname = 
+
+loggerprefix = logbook.Logger('s3logsync log')
+log = logbook.TimedRotatingFileHandler(cfg_logname, date_format='%Y-%m-%d')
+log.push_application()
+
+def sync_logs(s3folder, localstorage):
+	try:	
+		sync = subprocess.Popen(["/usr/local/bin/s4cmd.py", "sync","-s", s3folder, localstorage], stdout=subprocess.PIPE)
+		out = sync.communicate()[0]
+		sync.wait()
+		logger.info(out.upper())
+		logger.info('Sync completed.')
+	except subprocess.CalledProcessError, fail:
+		fail = logger.warn("Sync failed!, check error log.")
+
+sync_logs(cfg_s3folder, cfg_localstorage)
